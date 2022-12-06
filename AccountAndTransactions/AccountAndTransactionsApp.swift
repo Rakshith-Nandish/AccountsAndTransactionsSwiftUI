bfb9b79407631ebcaf0db5f7c18209cf8d9b61c9
Rakshith <nrakshith94@gmail.com>
2022-12-06 17:25:24 +0000

    Initial Commit

---
+++ AccountAndTransactions/AccountAndTransactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountAndTransactionsApp.swift
+//  AccountAndTransactions
+//
+//  Created by Rakshith on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AccountAndTransactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
